6e98e1272347e7fbe2868faca186eb629c2d6cf7
Dimitar Ivanov <biggie@abv.bg>
2019-05-01 14:14:23 +0300

    Update config.php

--- examples/config.php
+++ examples/config.php
@@ -1,23 +1,23 @@
 <?php
 return array(
     'Facebook' => array(
-        'client_id' => '2639671186106462',
-        'client_secret' => '4e7910f230639d9efac7b66a2fc26166',
+        'client_id' => '',
+        'client_secret' => '',
         'redirect_uri' => 'http://localhost/WebAuth/examples/callback-facebook.php',
     ),
     'Google' => array(
-        'client_id' => '182717856094-a6bil4q0cvism0frggtk0kjr83kf9ru2.apps.googleusercontent.com',
-        'client_secret' => 'wrqzZMmBe7ZCVjX_F_D8qRvQ',
+        'client_id' => '',
+        'client_secret' => '',
         'redirect_uri' => 'http://localhost/WebAuth/examples/callback-google.php',
     ),
     'LinkedIn' => array(
-        'client_id' => '78pgi7d8gyzotj',
-        'client_secret' => 'VSamGJZviQxGTSNG',
+        'client_id' => '',
+        'client_secret' => '',
         'redirect_uri' => 'http://localhost/WebAuth/examples/callback-linkedin.php',
     ),
     'Twitter' => array(
-        'client_id' => 'rBGQXmRvKphVI7oAdLiV2pUhj',
-        'client_secret' => 'pjOWZsC3c0fGh6H1syxYf0vEfj1QVzivj7sjnKFuC3ehM8KoQ5',
+        'client_id' => '',
+        'client_secret' => '',
         'redirect_uri' => 'http://localhost/WebAuth/examples/callback-twitter.php',
     ),
 );
